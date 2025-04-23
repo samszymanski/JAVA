f899f160c2b223e317f6c610405286aa6616a65d
Sam <samaraszymanski@gmail.com>
2025-04-23 13:09:48 -0300

    Add files via upload

---
+++ 1038 - Lanche/exercicio.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int codigo, quantidade;
+		double total;
+		
+		codigo = sc.nextInt();
+		quantidade = sc.nextInt();
+		
+		if (codigo == 1) {
+			total = quantidade * 4.0;
+		}
+		else if (codigo == 2) {
+			total = quantidade * 4.5;
+		}
+		else if (codigo == 3) {
+			total = quantidade * 5.0;
+		}
+		else if (codigo == 4) {
+			total = quantidade * 2.0;
+		}
+		else {
+			total = quantidade * 1.5;
+		}
+		
+		System.out.printf("Total: R$ %.2f%n", total);
+
+
+		sc.close();
+	}
+}
